b6072040a723556e86f09f53f9d470f1e962b78b
Ariel Montes <arielmontes1989@gmail.com>
2020-11-22 23:21:55 +0100

    Code Improvements

--- clock.js
+++ clock.js
@@ -1,38 +1,35 @@
-// Analog clock
-function Reloj() {
+// Analog Clock
+function Clock() {
     this.canvas=document.getElementsByTagName('canvas')[0];
     this.canvas.width=400;
     this.canvas.height=400;
     this.ctx = this.canvas.getContext('2d');
     this.ctx.scale(2, 2);
 
-    this.setHora();
+    this.setHour();
     var self=this;
-    this.intervalo = setInterval(function(){self.actualizar();},1000);
+    this.interval = setInterval(function(){self.update();},1000);
 }
-Reloj.prototype.setHora=function(){
-    var hora = new Date();
-    this.segundos=hora.getSeconds();
-    this.minutos=hora.getMinutes();
-    this.horas=(hora.getHours()>12?hora.getHours()-12:(hora.getHours()==0)?12:hora.getHours());
+Clock.prototype.setHour=function() {
+    var hour = new Date();
+    this.seconds=hour.getSeconds();
+    this.minutes=hour.getMinutes();
+    this.hours=(hour.getHours()>12?hour.getHours()-12:(hour.getHours()==0)?12:hour.getHours());
 
     this.rad12=2*Math.PI/12;
     this.rad60=2*Math.PI/60;
 }
-Reloj.prototype.incrementar=function()
-{
-    this.segundos=(++this.segundos)%60;
-    if (this.segundos==0)
-    {
-        this.minutos=(++this.minutos)%60;
-        if (this.minutos==0)
+Clock.prototype.increment=function() {
+    this.seconds=(++this.seconds)%60;
+    if (this.seconds==0) {
+        this.minutes=(++this.minutes)%60;
+        if (this.minutes==0)
         {
-            this.horas=(++this.horas)%12;
+            this.hours=(++this.hours)%12;
         }
     }
 }
-Reloj.prototype.dibujar=function()
-{
+Clock.prototype.draw=function() {
     this.ctx.clearRect(0,0,200,200);
     this.ctx.lineWidth = 2;
     this.ctx.strokeStyle = "transparent";
@@ -41,85 +38,84 @@ Reloj.prototype.dibujar=function()
     this.ctx.stroke();
 
     this.ctx.save();
-        this.ctx.translate(100,100);
+    this.ctx.translate(100,100);
 
-        this.ctx.font = "bold 20px monospace";
-        this.ctx.fillStyle="white";
-        this.ctx.fillText(12,-12,-70);
-        this.ctx.fillText(6,-6,82);
-        this.ctx.fillText(1,32,-58);
-        this.ctx.fillText(7,-45,70);
-        this.ctx.fillText(2,60,-32);
-        this.ctx.fillText(8,-72,46);
-        this.ctx.fillText(3,70,5);
-        this.ctx.fillText(9,-82,5);
-        this.ctx.fillText(4,60,47);
-        this.ctx.fillText(10,-71,-33);
-        this.ctx.fillText(5,33,72);
-        this.ctx.fillText(11,-47,-57);
+    this.ctx.font = "bold 20px monospace";
+    this.ctx.fillStyle="white";
+    this.ctx.fillText(12,-12,-70);
+    this.ctx.fillText(6,-6,82);
+    this.ctx.fillText(1,32,-58);
+    this.ctx.fillText(7,-45,70);
+    this.ctx.fillText(2,60,-32);
+    this.ctx.fillText(8,-72,46);
+    this.ctx.fillText(3,70,5);
+    this.ctx.fillText(9,-82,5);
+    this.ctx.fillText(4,60,47);
+    this.ctx.fillText(10,-71,-33);
+    this.ctx.fillText(5,33,72);
+    this.ctx.fillText(11,-47,-57);
 
-        for (var i=0;i<30;i++)
-        {
-            this.ctx.save();
-                this.ctx.rotate(i*this.rad60);
-                this.ctx.lineWidth = 2;
-                this.ctx.strokeStyle = "white";
-                this.ctx.beginPath();
-                this.ctx.moveTo(0,-90);
-                this.ctx.lineTo(0,-95);
-                this.ctx.stroke();
-                this.ctx.beginPath();
-                this.ctx.moveTo(0,90);
-                this.ctx.lineTo(0,95);
-                this.ctx.stroke();
-            this.ctx.restore();
-        }
-        for (var i=0;i<6;i++)
-        {
-            this.ctx.save();
-                this.ctx.rotate(i*this.rad12);
-                this.ctx.lineWidth = 5;
-                this.ctx.strokeStyle = "white";
-                this.ctx.beginPath();
-                this.ctx.moveTo(0,-85);
-                this.ctx.lineTo(0,-95);
-                this.ctx.stroke();
-                this.ctx.beginPath();
-                this.ctx.moveTo(0,85);
-                this.ctx.lineTo(0,95);
-                this.ctx.stroke();
-            this.ctx.restore();
-        }
-
+    for (var i=0;i<30;i++) {
         this.ctx.save();
-            this.ctx.rotate(this.horas*this.rad12+this.minutos/60*this.rad12);
-            this.ctx.lineWidth = 7;
-            this.ctx.strokeStyle = "white";
-            this.ctx.beginPath();
-            this.ctx.moveTo(0,7);
-            this.ctx.lineTo(0,-60);
-            this.ctx.stroke();
+        this.ctx.rotate(i*this.rad60);
+        this.ctx.lineWidth = 2;
+        this.ctx.strokeStyle = "white";
+        this.ctx.beginPath();
+        this.ctx.moveTo(0,-90);
+        this.ctx.lineTo(0,-95);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(0,90);
+        this.ctx.lineTo(0,95);
+        this.ctx.stroke();
         this.ctx.restore();
+    }
 
+    for (var i=0;i<6;i++) {
         this.ctx.save();
-            this.ctx.rotate(this.minutos*this.rad60+this.segundos/60*this.rad60);
-            this.ctx.lineWidth = 4;
-            this.ctx.strokeStyle = "white";
-            this.ctx.beginPath();
-            this.ctx.moveTo(0,10);
-            this.ctx.lineTo(0,-70);
-            this.ctx.stroke();
+        this.ctx.rotate(i*this.rad12);
+        this.ctx.lineWidth = 5;
+        this.ctx.strokeStyle = "white";
+        this.ctx.beginPath();
+        this.ctx.moveTo(0,-85);
+        this.ctx.lineTo(0,-95);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(0,85);
+        this.ctx.lineTo(0,95);
+        this.ctx.stroke();
         this.ctx.restore();
+    }
 
-        this.ctx.save();
-            this.ctx.rotate(this.segundos*this.rad60);
-            this.ctx.lineWidth = 1;
-            this.ctx.strokeStyle = "red";
-            this.ctx.beginPath();
-            this.ctx.moveTo(0,15);
-            this.ctx.lineTo(0,-80);
-            this.ctx.stroke();
-        this.ctx.restore();
+    this.ctx.save();
+    this.ctx.rotate(this.hours*this.rad12+this.minutes/60*this.rad12);
+    this.ctx.lineWidth = 7;
+    this.ctx.strokeStyle = "white";
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,7);
+    this.ctx.lineTo(0,-60);
+    this.ctx.stroke();
+    this.ctx.restore();
+
+    this.ctx.save();
+    this.ctx.rotate(this.minutes*this.rad60+this.seconds/60*this.rad60);
+    this.ctx.lineWidth = 4;
+    this.ctx.strokeStyle = "white";
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,10);
+    this.ctx.lineTo(0,-70);
+    this.ctx.stroke();
+    this.ctx.restore();
+
+    this.ctx.save();
+    this.ctx.rotate(this.seconds*this.rad60);
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = "red";
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,15);
+    this.ctx.lineTo(0,-80);
+    this.ctx.stroke();
+    this.ctx.restore();
 
     this.ctx.restore();
     
@@ -129,12 +125,11 @@ Reloj.prototype.dibujar=function()
     this.ctx.fill();
     
 }
-Reloj.prototype.actualizar=function()
-{
-    this.incrementar();
-    this.dibujar();
+Clock.prototype.update=function() {
+    this.increment();
+    this.draw();
 
-    console.log(this.horas+":"+this.minutos+":"+this.segundos);
+    // console.log(this.hours+":"+this.minutes+":"+this.seconds);
 }
 
-var reloj = new Reloj();
+var clock = new Clock();
