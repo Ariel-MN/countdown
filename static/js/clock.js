810e074a677ff3e333a72c6a80e3e811c874dde5
Ariel Montes <arielmontes1989@gmail.com>
2020-11-23 13:38:17 +0100

    Code and aesthetic improvements

--- static/js/clock.js
+++ static/js/clock.js
@@ -1,5 +1,6 @@
 // Analog Clock
 function Clock() {
+    // Canvas configuration
     this.canvas=document.getElementsByTagName('canvas')[0];
     this.canvas.width=400;
     this.canvas.height=400;
@@ -10,6 +11,7 @@ function Clock() {
     var self=this;
     this.interval = setInterval(function(){self.update();},1000);
 }
+// Clock configuration
 Clock.prototype.setHour=function() {
     var hour = new Date();
     this.seconds=hour.getSeconds();
@@ -40,6 +42,7 @@ Clock.prototype.draw=function() {
     this.ctx.save();
     this.ctx.translate(100,100);
 
+    // Clock numbers font and position
     this.ctx.font = "bold 20px serif";
     this.ctx.fillStyle="white";
     this.ctx.fillText(12,-10,-69);
@@ -55,6 +58,7 @@ Clock.prototype.draw=function() {
     this.ctx.fillText(5,33,72);
     this.ctx.fillText(11,-47,-57);
 
+    // Small indices of watch dial
     for (var i=0;i<30;i++) {
         this.ctx.save();
         this.ctx.rotate(i*this.rad60);
@@ -71,6 +75,7 @@ Clock.prototype.draw=function() {
         this.ctx.restore();
     }
 
+    // Large indices of watch dial
     for (var i=0;i<6;i++) {
         this.ctx.save();
         this.ctx.rotate(i*this.rad12);
@@ -87,40 +92,50 @@ Clock.prototype.draw=function() {
         this.ctx.restore();
     }
 
+    // Minutes hand of the clock
+    this.ctx.save();
+    this.ctx.rotate(this.minutes*this.rad60+this.seconds/60*this.rad60);
+    this.ctx.lineWidth = 4;
+    this.ctx.strokeStyle = "#c0c0c0";
+    this.ctx.beginPath();
+    this.ctx.lineCap = "round";
+    this.ctx.moveTo(0,03);
+    this.ctx.lineTo(0,-70);
+    this.ctx.stroke();
+    this.ctx.restore();
+
+    // Hours hand of the clock
     this.ctx.save();
     this.ctx.rotate(this.hours*this.rad12+this.minutes/60*this.rad12);
     this.ctx.lineWidth = 6;
     this.ctx.strokeStyle = "white";
     this.ctx.beginPath();
     this.ctx.lineCap = "round";
-    this.ctx.moveTo(0,7);
+    this.ctx.moveTo(0,3);
     this.ctx.lineTo(0,-55);
     this.ctx.stroke();
     this.ctx.restore();
-
+    // Head of the hours hand
     this.ctx.save();
-    this.ctx.rotate(this.minutes*this.rad60+this.seconds/60*this.rad60);
-    this.ctx.lineWidth = 4;
-    this.ctx.strokeStyle = "white";
+    this.ctx.fillStyle = "white";
     this.ctx.beginPath();
-    this.ctx.lineCap = "round";
-    this.ctx.moveTo(0,10);
-    this.ctx.lineTo(0,-70);
-    this.ctx.stroke();
+    this.ctx.arc(0, 0, 6, 0, 2 * Math.PI, false);
+    this.ctx.fill();
     this.ctx.restore();
-
+    
+    // Seconds hand of the clock
     this.ctx.save();
     this.ctx.rotate(this.seconds*this.rad60);
     this.ctx.lineWidth = 1;
     this.ctx.strokeStyle = "OrangeRed";
     this.ctx.beginPath();
+    this.ctx.lineCap = "round";
     this.ctx.moveTo(0,15);
     this.ctx.lineTo(0,-80);
     this.ctx.stroke();
     this.ctx.restore();
-
     this.ctx.restore();
-    
+    // Head of the seconds hand
     this.ctx.fillStyle = "OrangeRed";
     this.ctx.beginPath();
     this.ctx.arc(100, 100, 4, 0, 2 * Math.PI, false);
@@ -130,7 +145,6 @@ Clock.prototype.draw=function() {
 Clock.prototype.update=function() {
     this.increment();
     this.draw();
-
     // console.log(this.hours+":"+this.minutes+":"+this.seconds);
 }
 
--- static/js/countdown.js
+++ static/js/countdown.js
@@ -1,5 +1,5 @@
 // Get day number of the week, 4 is Thursday and 6 is Saturday
-var dateObj = new Date(); // For testing use Date("Dec 30, 2021 15:30:00")
+var dateObj = new Date();
 var weekdayNumber = dateObj.getDay();
 
 // Run ContDown if is Thursday or Saturday
@@ -7,13 +7,14 @@ var countDownDate = null;
 if (weekdayNumber == 4) {
     // Thursday time to count down
     var countDownDate = dateObj;
-    countDownDate.setHours(19,30,0); // Meeting starts at 7:30 pm
+    countDownDate.setHours(19,30,1); // Thursday meeting starts at 7:30 pm
 } else if (weekdayNumber == 6) {
     // Saturday time to count down
     var countDownDate = dateObj;
-    countDownDate.setHours(17,30,0); // Meeting starts at 5:30 pm
+    countDownDate.setHours(17,30,1); // Saturday meeting starts at 5:30 pm
 } else {
-    document.getElementById("count").innerHTML = 'No hay una reunión programada para hoy';
+    // Notifies that there are no meetings
+    document.getElementById("count").innerHTML = '<h4 class="text-muted">No hay una reunión programada para hoy</h4>';
 }
 
 // ContDown
@@ -33,24 +34,43 @@ if (countDownDate) {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-        // Display the result in the element with id="count"
-        if (days != 0) { 
-            document.getElementById("count").innerHTML = days + " dias " + hours + "h " + minutes + "m " + seconds + "s ";
-        } else if (hours != 0) {
-            document.getElementById("count").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
-        } else {
-            document.getElementById("count").innerHTML = minutes + "m " + seconds + "s ";
-            if (minutes < 3) {
-                document.getElementById("alert").innerHTML = "Dentro de poco la reunión empieza";
-            }
-        }
 
-        // If the count down is finished, write some text
-        if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("alert").innerHTML = "";
-        document.getElementById("count").innerHTML = "Reunión Iniciada";
+        // Display the result in the element with id="count" and id="alert"
+        switch(true) {
+            case days > 0:
+                // Count the days
+                document.getElementById("count").innerHTML = '<h3>' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ' + '</h3>';
+                break;
+            case hours > 0:
+                // Count the hours
+                document.getElementById("count").innerHTML = '<h3>' + hours + 'h ' + minutes + 'm ' + seconds + 's ' + '</h3>';
+                break;
+            case minutes > 0:
+                // Warns that the meeting is about to start, count the minutes
+                document.getElementById("count").innerHTML = '<h3>' + minutes + 'm ' + seconds + 's ' + '</h3>';
+                if (minutes < 3) {
+                    document.getElementById("alert").innerHTML = '<h3>La reunión comenzará dentro de poco</h3>';
+                }
+                break;
+            case seconds > 0:
+                // Warns that the meeting is about to start, count the seconds in orange
+                document.getElementById("alert").innerHTML = '<h3>La reunión comenzará dentro de poco</h3>';
+                document.getElementById("count").innerHTML = '<h3 class="text-orange">' + seconds + 's ' + '</h3>';
+                break;
+            case hours < -2:
+                // If the meeting has finished, write some text
+                document.getElementById("count").innerHTML = '';
+                document.getElementById("alert").innerHTML = '<h3>Reunión Terminada</h3>';
+                break;
+            case seconds <= 0:
+                // If the meeting has started, write some text
+                document.getElementById("count").innerHTML = '';
+                document.getElementById("alert").innerHTML = '<h3>Reunión Iniciada</h3>';
+        }
+    
+        // Stops the function after 3 hours from the start of the meeting
+        if (hours < -3) {
+            clearInterval(x);
         }
 
     }, 1000);
